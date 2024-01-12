5b8b130ac55b44aa941fec070f03d58dfbb8fd9a
neapieroelie <nea.pieroelie@gmail.com>
2024-01-11 22:46:07 -0800

    adding all files into exercise-2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Exercise 1
+## Created by
+- Nea
+## Description
+- In class we learned how to create components in the main branch and import it into the home page. 
---
+++ components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return(
+        <footer className={styles.footer}>
+            Created by Nea Pieroelie
+        </footer>
+    )
+}
